3e3914ff8dadb4da4fe7088bcb8e2ab2151ff14c
Turjo99 <imturjo27@gmail.com>
2022-11-13 10:08:17 +0600

    Basic layout done and components created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes";
 
 function App() {
   return (
     <div className="">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
