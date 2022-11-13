ce42cadecb9cb68904b8603e9b595a6dbed221cf
Turjo99 <imturjo27@gmail.com>
2022-11-13 22:57:52 +0600

    MakeAppointment Section done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import AnotherFeature from "./AnotherFeature";
 import Features from "./Features";
 import Hero from "./Hero";
+import MakeAppointment from "./MakeAppointment";
 import OurService from "./OurService";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Features></Features>
       <OurService></OurService>
       <AnotherFeature></AnotherFeature>
+      <MakeAppointment></MakeAppointment>
     </div>
   );
 };
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../Components/Pages/Home";
+import Home from "../Components/Home/Home";
 import Main from "../Layout/Main";
 
 export const router = createBrowserRouter([
