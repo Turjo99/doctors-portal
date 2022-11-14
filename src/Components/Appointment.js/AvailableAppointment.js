1418476d8db3ba95a236bdaf111e437f34a2c898
Turjo99 <imturjo27@gmail.com>
2022-11-14 22:25:24 +0600

    Appointment Services added

--- src/Components/Appointment.js/AvailableAppointment.js
+++ src/Components/Appointment.js/AvailableAppointment.js
@@ -1,12 +1,32 @@
 import { format } from "date-fns";
-import React from "react";
+import { da } from "date-fns/locale";
+import React, { useEffect, useState } from "react";
+import AppointmentModal from "./AppointmentModal";
+import AppointmentOption from "./AppointmentOption";
 
 const AvailableAppointment = ({ selected }) => {
+  const [appointmentOptions, setAppointmentOptions] = useState([]);
+  useEffect(() => {
+    fetch("appointmentOptions.json")
+      .then((res) => res.json())
+      .then((data) => setAppointmentOptions(data));
+  }, []);
   return (
     <div className="my-10">
-      <p className="text-center text-primary font-bold">
+      {console.log(appointmentOptions)}
+      <p className="text-center text-primary font-bold my-10">
         Available Appointments on {format(selected, "PP")}
       </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {appointmentOptions.map((option) => (
+          <AppointmentOption
+            key={option._id}
+            option={option}
+          ></AppointmentOption>
+        ))}
+      </div>
+
+      <AppointmentModal></AppointmentModal>
     </div>
   );
 };
